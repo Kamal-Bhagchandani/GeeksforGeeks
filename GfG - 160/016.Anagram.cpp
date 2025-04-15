e498d3ea24ac918b57d3caeb7aab89aa35dedce4
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2025-04-15 23:37:14 +0530

    Day 16

---
+++ GfG - 160/016.Anagram.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+/*
+Question Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-gfg-160/problem/anagram-1587115620
+*/
+
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // Your code here
+        unordered_map<char,int> m;
+        for (int i=0;i<s1.length();i++){
+            m[s1[i]]++;
+        }
+        for (int i=0;i<s2.length();i++){
+            m[s2[i]]--;
+        }
+        for(auto i:m){
+            if (i.second!=0) return false;
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
