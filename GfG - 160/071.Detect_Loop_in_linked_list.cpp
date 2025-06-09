194aceba17a043af32f3bd9aa1e1132c6ea6a628
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2025-06-09 23:03:17 +0530

    Day 71

---
+++ GfG - 160/071.Detect_Loop_in_linked_list.cpp
@@ -0,0 +1,30 @@
+/*
+Question Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/linked-list-gfg-160/problem/detect-loop-in-linked-list
+*/
+
+/*
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution {
+  public:
+    // Function to check if the linked list has a loop.
+    bool detectLoop(Node* head) {
+        // your code here
+        Node* slow=head;
+        Node* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+};
