2c44fe65de03a14e750003a72b6866920a2a345e
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2025-08-09 00:17:41 +0530

    Day 131

---
+++ GfG - 160/131.Boolean_Parenthesization.cpp
@@ -0,0 +1,52 @@
+/*
+Question Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/dynamic-programming-gfg-160/problem/boolean-parenthesization5610
+*/
+
+// User function Template for C++
+class Solution {
+  public:
+    bool evaluate(int b1,int b2,char op){
+        if(op=='&') return b1&b2;
+        if(op=='|') return b1|b2;
+        return b1^b2;
+    }
+    int countWays(string &s) {
+        // code here
+        int n=s.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,0)));
+        for(int i=0;i<n;i+=2){
+            dp[i][i][1]=(s[i]=='T');
+            dp[i][i][0]=(s[i]=='F');
+        }
+        for(int len=2;len<n;len+=2){
+            for(int i=0;i<n-len;i+=2){
+                int j=i+len;
+                dp[i][j][0]=dp[i][j][1]=0;
+                for(int k=i+1;k<j;k+=2){
+                    char op=s[k];
+                    int leftTrue=dp[i][k-1][1],leftFalse=dp[i][k-1][0];
+                    int rightTrue=dp[k+1][j][1],rightFalse=dp[k+1][j][0];
+                    if(evaluate(1,1,op))
+                        dp[i][j][1]+=leftTrue*rightTrue;
+                    if(evaluate(1,0,op))
+                        dp[i][j][1]+=leftTrue*rightFalse;
+                    if(evaluate(0,1,op))
+                        dp[i][j][1]+=leftFalse*rightTrue;
+                    if(evaluate(0,0,op))
+                        dp[i][j][1]+=leftFalse*rightFalse;
+                        
+                    if(!evaluate(1,1,op))
+                        dp[i][j][0]+=leftTrue*rightTrue;
+                    if(!evaluate(1,0,op))
+                        dp[i][j][0]+=leftTrue*rightFalse;
+                    if(!evaluate(0,1,op))
+                        dp[i][j][0]+=leftFalse*rightTrue;
+                    if(!evaluate(0,0,op))
+                        dp[i][j][0]+=leftFalse*rightFalse;
+                        
+                }
+            }
+        }
+        return dp[0][n-1][1];
+    }
+};
