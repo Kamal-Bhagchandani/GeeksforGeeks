1a2d00afdb066bfb3f08264934e349c2c9794327
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2025-08-11 23:57:38 +0530

    Day 134

---
+++ GfG - 160/134.Activity_Selection.cpp
@@ -0,0 +1,32 @@
+/*
+Question Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/greedy-gfg-160/problem/activity-selection-1587115620
+*/
+
+class Solution {
+  public:
+    int activitySelection(vector<int> &start, vector<int> &finish) {
+        // code here
+        vector<vector<int>> arr;
+        
+        for(int i=0;i<start.size();i++){
+            arr.push_back({start[i],finish[i]});
+        }
+        
+        sort(arr.begin(),arr.end(),
+        [](const vector<int>& a,const vector<int>& b){
+            return a[1]<b[1];
+        });
+        
+        int count=1;
+        int j=0;
+        
+        for(int i=1;i<arr.size();i++){
+            if(arr[i][0]>arr[j][1]){
+                count++;
+                j=i;
+            }
+        }
+        
+        return count;
+    }
+};
