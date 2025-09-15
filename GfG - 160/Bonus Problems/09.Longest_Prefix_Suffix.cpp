66ae1cb0ae1b1b21ad80b03b40b10432f66859b7
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2025-09-15 23:08:53 +0530

    Bonus Problem 9

---
+++ GfG - 160/Bonus Problems/09.Longest_Prefix_Suffix.cpp
@@ -0,0 +1,31 @@
+/*
+Question Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-bonus-problems/problem/longest-prefix-suffix2527
+*/
+
+class Solution {
+  public:
+    int getLPSLength(string &s) {
+        // code here
+        int n = s.length();
+        vector<int> lps(n,0);
+        int len=0, i=1;
+        
+        while(i<n){
+            if(s[i]==s[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps[n-1];
+    }
+};
